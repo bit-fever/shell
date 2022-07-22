9ae44b62ca15bd77ff63f7f3865ace47b1325332
acarboni <andrea.carboni@hpe.com>
2022-07-23 01:01:53 +0200

    Initial code

---
+++ pkg/command/exchange/exchange.go
@@ -0,0 +1,61 @@
+//=============================================================================
+/*
+Copyright © 2022 Andrea Carboni andrea.carboni71@gmail.com
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*/
+//=============================================================================
+
+package exchange
+
+import (
+	"fmt"
+	"github.com/bit-fever/shell/pkg/command"
+
+	"github.com/spf13/cobra"
+)
+
+//=============================================================================
+
+var exchangeCmd = &cobra.Command{
+	Use:   "exchange",
+	Short: "Run exchange related commands",
+	Long:  `Run commands affecting the exchange server, like list, add, remove, etc...`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Println("exchange called")
+	},
+}
+
+//=============================================================================
+
+func init() {
+	command.RootCmd.AddCommand(exchangeCmd)
+
+	// Here you will define your flags and configuration settings.
+
+	// Cobra supports Persistent Flags which will work for this command
+	// and all subcommands, e.g.:
+	// exchangeCmd.PersistentFlags().String("foo", "", "A help for foo")
+
+	// Cobra supports local flags which will only run when this command
+	// is called directly, e.g.:
+	// exchangeCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+}
+
+//=============================================================================
